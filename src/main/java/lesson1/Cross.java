8f872d01f107d9b8d7f78a5677e86d6264aeac2e
nikolaitch <nikolaitch71@gmail.com>
2022-11-13 20:20:32 +0300

    Новая задача

---
+++ src/main/java/lesson1/Cross.java
@@ -0,0 +1,16 @@
+package lesson1;
+
+
+
+public class Cross extends Obstacle {
+    private int distance;
+
+    public Cross(int distance) {
+        this.distance = distance;
+    }
+
+    @Override
+    public void doIt(Competitor competitor) {
+        competitor.run(distance);
+    }
+}
